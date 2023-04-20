1856895f66a2b9d4bd502d39a3345a0bbdd73eaa
meatisdelicious <ssquerel@gmail.com>
2023-04-20 15:43:38 +0200

    adding minor changes

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,7 +1,9 @@
 import Banner from './Banner'
+import Cart from '.Cart'
 
 function App() {
   return <Banner />
+  return <Cart />
 }
 
 export default App;
--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -2,4 +2,3 @@ function Banner(){
     return <h1>La maison jungle</h1>
 }
 export default Banner
-
