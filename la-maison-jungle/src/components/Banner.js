c2251a175bc219486f74c9118164deacfacc9b52
meatisdelicious <ssquerel@gmail.com>
2023-04-20 16:29:24 +0200

    adding code to banner.css

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,5 +1,10 @@
 function Banner(){
+    <div className='lmj-banner'>
+        <h1> La maison jungle </h1>
+    </div>
+    
     const title ='La maison de la jungle'
-    return <h1> La maison jungle </h1>
+    return <h1> La maison jungle </h1> 
 }
+
 export default Banner
