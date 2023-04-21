37a8a9812508f662985ca565364344bd73421df3
meatisdelicious <ssquerel@gmail.com>
2023-04-21 17:07:43 +0200

    minor changes

--- simple-react-app1/src/App.js
+++ simple-react-app1/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    console.log("Data from APi--", data.Search);
   };
   useEffect(() => {
     searchMovies("Spiderman");
