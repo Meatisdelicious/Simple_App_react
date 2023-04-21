19bacc3726425730def0f40b97717bb7ce24ffc6
meatisdelicious <ssquerel@gmail.com>
2023-04-21 16:56:28 +0200

    validated api key

--- simple-react-app1/src/App.js
+++ simple-react-app1/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
 
-// 12dc7cfc
-
-const API_URL = "http://www.omdbapi.com?apikey=12dc7cfc";
+// const API_URL = "http://www.omdbapi.com?apikey=12dc7cfc";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=12dc7cfc";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return <h1> App </h1>;
 };
 
