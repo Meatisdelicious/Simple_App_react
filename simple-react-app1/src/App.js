69a2272ac2191fef9bffee227b3c8aad67dece3e
meatisdelicious <ssquerel@gmail.com>
2023-04-24 12:22:23 +0200

    adding new placeholder

--- simple-react-app1/src/App.js
+++ simple-react-app1/src/App.js
@@ -42,7 +42,22 @@ const App = () => {
 
       <div className="container">
         <div className="movie">
-          <p>{movie1.Year}</p>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+          </div>
         </div>
       </div>
     </div>
