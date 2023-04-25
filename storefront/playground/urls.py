0e49dae478360b064207f4fe1b45ee2c314f8017
meatisdelicious <ssquerel@gmail.com>
2023-04-25 10:51:09 +0200

    adding url to storefront app

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # from the currnet folder, we import views file
 from . import views
 
-# URLConfiguration
+# URLConf
 urlpatterns = [
-    path('playground/hello', views.say_hello)
+    path('hello/', views.say_hello)
 ]
