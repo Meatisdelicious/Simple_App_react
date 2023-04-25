7a871680a523c1e4b42b93939e76ac82430f4a19
meatisdelicious <ssquerel@gmail.com>
2023-04-25 13:35:49 +0200

    adding changes

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -5,5 +5,12 @@ from django.http import HttpResponse
 # request handler
 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+
 def say_hello(request):
+    x = calculate()
     return render(request, 'hello.html', {'name': 'Adrien'})
