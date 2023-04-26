b1918ab8be1e8425442a6b9d3463a9ef5e0903d0
meatisdelicious <ssquerel@gmail.com>
2023-04-26 13:38:42 +0200

    adding http response

--- django_restapi/django_restapi/views.py
+++ django_restapi/django_restapi/views.py
@@ -2,14 +2,24 @@ from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # something you put above a function to describe it in some way
 @api_view('GET', 'POST')
 def drink_list(request):
-    # get all the drinks from class Drink
-    drinks = Drink.objects.all()
-    # serialize them
-    serializer = DrinkSerializer(drinks, many=True)
-    # return json
-    return JsonResponse({"drinks": serializer.data})
+
+    if request.method == 'GET':
+        # get all the drinks from class Drink
+        drinks = Drink.objects.all()
+        # serialize them
+        serializer = DrinkSerializer(drinks, many=True)
+        # return json
+        return JsonResponse({"drinks": serializer.data})
+
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
