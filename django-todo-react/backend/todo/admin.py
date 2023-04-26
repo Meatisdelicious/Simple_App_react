7a55aa5c20cdb05be5a4b173690bbb8eac8635e5
meatisdelicious <ssquerel@gmail.com>
2023-04-26 16:58:20 +0200

    adding imports to settings.py from the backend

--- django-todo-react/backend/todo/admin.py
+++ django-todo-react/backend/todo/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-
 from .models import Todo
 
 
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
+# Register your models here.
 
-# register your modle here :
-admin.site.register(Todo.TodoAdmin)
+
+admin.site.register(Todo, TodoAdmin)
