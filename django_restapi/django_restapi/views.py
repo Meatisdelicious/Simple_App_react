c444869ce41130f9a9ebccf1698dbab7649882e6
meatisdelicious <ssquerel@gmail.com>
2023-04-26 12:18:55 +0200

    get, post request

--- django_restapi/django_restapi/views.py
+++ django_restapi/django_restapi/views.py
@@ -1,12 +1,15 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework import api_view
+# something you put above a function to describe it in some way
 
 
+@api_view('GET', 'POST')
 def drink_list(request):
     # get all the drinks from class Drink
     drinks = Drink.objects.all()
     # serialize them
     serializer = DrinkSerializer(drinks, many=True)
     # return json
-    return JsonResponse(serializer.data)
+    return JsonResponse({"drinks": serializer.data})
