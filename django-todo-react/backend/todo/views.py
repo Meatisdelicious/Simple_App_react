0ca5e441911cdaa0bffbcb36fa9f2b10d4039ede
meatisdelicious <ssquerel@gmail.com>
2023-04-26 17:29:44 +0200

    adding comments to views

--- django-todo-react/backend/todo/views.py
+++ django-todo-react/backend/todo/views.py
@@ -4,6 +4,10 @@ from .serializers import TodoSerializer
 from .models import Todo
 
 
+# The viewsets base class provides the implementation for
+#  CRUD operations by default. This code specifies the serializer_class and the queryset.
+
+
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.Objects.all()
