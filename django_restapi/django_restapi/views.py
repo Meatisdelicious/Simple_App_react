7d46e08c6da36709cdf3ab740bbb86a8633760c3
meatisdelicious <ssquerel@gmail.com>
2023-04-26 13:33:44 +0200

    adding @api_viw

--- django_restapi/django_restapi/views.py
+++ django_restapi/django_restapi/views.py
@@ -1,10 +1,10 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
-from rest_framework import api_view
+from rest_framework.decorators import api_view
+
+
 # something you put above a function to describe it in some way
-
-
 @api_view('GET', 'POST')
 def drink_list(request):
     # get all the drinks from class Drink
