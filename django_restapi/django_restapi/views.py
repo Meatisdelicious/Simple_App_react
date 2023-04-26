b58ff9a2b9164e3754df8f0bf8ac6f9350a78cc0
meatisdelicious <ssquerel@gmail.com>
2023-04-26 11:44:23 +0200

    adding serializer

---
+++ django_restapi/django_restapi/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+
+def drink_list(request):
+    # get all the drinks from class Drink
+    drinks = Drink.objects.all()
+    # serialize them
+    serializer = DrinkSerializer(drinks, many=True)
+    # return json
+    return JsonResponse(serializer.data)
