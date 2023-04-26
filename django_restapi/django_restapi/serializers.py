3e41893d9ee8ef28af813479894da50b969f3deb
meatisdelicious <ssquerel@gmail.com>
2023-04-26 11:37:35 +0200

    adding views and drinkserializer

---
+++ django_restapi/django_restapi/serializers.py
@@ -0,0 +1,9 @@
+# describes the process from going to a python object to JSON
+from rest_framework import serializers
+from .models import Drink
+
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
